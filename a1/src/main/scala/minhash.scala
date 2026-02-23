7e2aff201a9aba69c00e5b5ca37c56040dc8bcb3
Vaughn McManamna <vaughnmcmanamna03@gmail.com>
2026-02-23 00:08:21 -0800

    Implement shingle_line and compute_jaccard_pair

--- a1/src/main/scala/minhash.scala
+++ a1/src/main/scala/minhash.scala
@@ -6,12 +6,41 @@ import org.apache.spark.rdd.RDD
 object minhash {
 
 
-  def shingle_line(stList: List[String], shingleSize:Int):Shingled_Record = ???
+  def shingle_line(stList: List[String], shingleSize:Int):Shingled_Record = {
+    if (stList.isEmpty) throw new IllegalArgumentException("...")
+    
+    val id = stList.head
+    val words = stList.tail
+    val nWords = words.size
+
+    val shingles =
+      if (nWords < shingleSize) None
+      else Some(
+        words
+          .sliding(shingleSize)
+          .map(utils.stringList_to_hash)
+          .toSet
+      )
+    Shingled_Record(id, nWords, shingles)
+  }
 
   def minhash_record(r: Shingled_Record, hashFuncs: List[Hash_Func]): Min_Hash_Record = ???
 
+  def compute_jaccard_pair(a:Shingled_Record, b: Shingled_Record): Similarity = {
+    if (a.shingles.isEmpty || b.shingles.isEmpty) throw new IllegalArgumentException("...")
 
-  def compute_jaccard_pair(a:Shingled_Record, b: Shingled_Record): Similarity = ???
+    val setA = a.shingles.get
+    val setB = b.shingles.get
+
+    val intersection = setA.intersect(setB)
+    val union = setA.union(setB)
+
+    val sim = intersection.size.toDouble / union.size.toDouble
+
+    Similarity(a.id, b.id, sim)
+  }
+
+
 
   def find_jaccard_matches(records: RDD[Shingled_Record], minSimilarity:Double): Matches = ???
 
